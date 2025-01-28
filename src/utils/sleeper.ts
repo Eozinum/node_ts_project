c1fc932ccee64da3f5cd8ddae8c0d2266d843d8f
Eozinum <eozinum@gmail.com>
2025-01-28 22:13:57 +0200

    add link to the doc

--- src/utils/sleeper.ts
+++ src/utils/sleeper.ts
@@ -1,5 +1,5 @@
 import { setTimeout as nodeTimeout } from 'node:timers/promises'
 
-export const sleep = (ms: number) => nodeTimeout(ms) // This sleeper works in node 16 and above
+export const sleep = (ms: number) => nodeTimeout(ms) // This sleeper works in node 15 and above https://nodejs.org/api/timers.html#timerspromisessettimeoutdelay-value-options
 
 export const sleep2 = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
